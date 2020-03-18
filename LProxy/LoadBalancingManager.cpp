23d798b70a44e3b7ca0f958656d161460c577f08
xy12423 <xy12423@gmail.com>
2020-03-18 22:25:00 +0800

    Add receive segment

--- LProxy/LoadBalancingManager.cpp
+++ LProxy/LoadBalancingManager.cpp
@@ -10,27 +10,48 @@ Wnd     1b 13
 Len     2b 14
 */
 
-void LoadBalancingManager::VirtualConnection::AppendSendRequest(uint8_t flags, const char *payload, uint16_t payloadSize, SendRequestCallback &&completeHandler)
+void LoadBalancingManager::VirtualConnection::AppendSendSegment(uint8_t flags, const char *payload, uint16_t payloadSize, SegmentCallback &&completeHandler)
 {
-	if (payloadSize == 0)
-		return; //Empty data segment is not allowed
-	std::shared_ptr<SendRequest> sendingSegment = std::make_shared<SendRequest>(ioContext_, true);
+	std::unique_lock<std::recursive_mutex> lock(mutex_);
+	if (closed)
+		return;
+
+	bool isAck = (flags & ACK) || (flags & RST);
+	uint32_t sendSegmentId = sendSegmentIdNext_;
+	if (!isAck)
+		++sendSegmentIdNext_; //ACKs don't take new segid
+
+	lock.unlock();
+
+	std::shared_ptr<SendSegment> sendingSegment = std::make_shared<SendSegment>(ioContext_, sendSegmentId, isAck);
 	sendingSegment->data.resize(16 + payloadSize);
 	char *data = sendingSegment->data.data();
 	uint32_t virtualConnectionIdLE = boost::endian::native_to_little(virtualConnectionId_);
 	memcpy(data, &virtualConnectionIdLE, 4);
-	uint32_t sendSegmentIdNextLE = boost::endian::native_to_little(sendSegmentIdNext_++);
-	memcpy(data + 4, &sendSegmentIdNextLE, 4);
+	uint32_t sendSegmentIdLE = boost::endian::native_to_little(sendSegmentId);
+	memcpy(data + 4, &sendSegmentIdLE, 4);
 	data[12] = flags;
 	uint16_t payloadSizeLE = boost::endian::native_to_little(payloadSize);
 	memcpy(data + 14, &payloadSizeLE, 2);
+	memcpy(data + 16, payload, payloadSize);
 
-	std::unique_lock<std::recursive_mutex> lock(mutex_);
+	lock.lock();
 	if (closed)
 		return;
-	if (sendSegments_.size() >= sendWindow_)
+
+	if (shutdownFlags & SHUTDOWN_SEND)
 	{
-		std::shared_ptr<SendRequestCallback> callback = std::make_shared<SendRequestCallback>(completeHandler);
+		completeHandler(ERR_OPERATION_FAILURE);
+		return;
+	}
+	if (flags & FIN)
+		shutdownFlags |= SHUTDOWN_SEND;
+	if (flags & RST)
+		shutdownFlags |= SHUTDOWN_COMPLETE;
+	if (sendSegmentId - sendLaskAck_ > sendWindow_)
+	{
+		std::shared_ptr<SegmentCallback> callback = std::make_shared<SegmentCallback>(completeHandler);
+		sendSegmentIdPending_ = sendSegmentId;
 		sendCallback_ = [this, sendingSegment, callback](error_code err)
 		{
 			if (err)
@@ -45,7 +66,7 @@ void LoadBalancingManager::VirtualConnection::AppendSendRequest(uint8_t flags, c
 					return;
 				sendSegments_.push_back(std::move(sendingSegment));
 				lock.unlock();
-				BeginSendRequest();
+				BeginSendSegment();
 				(*callback)(0);
 			});
 		};
@@ -54,12 +75,12 @@ void LoadBalancingManager::VirtualConnection::AppendSendRequest(uint8_t flags, c
 	{
 		sendSegments_.push_back(std::move(sendingSegment));
 		lock.unlock();
-		BeginSendRequest();
+		BeginSendSegment();
 		completeHandler(0);
 	}
 }
 
-void LoadBalancingManager::VirtualConnection::BeginSendRequest()
+void LoadBalancingManager::VirtualConnection::BeginSendSegment()
 {
 	std::unique_lock<std::recursive_mutex> lock(mutex_);
 	if (closed)
@@ -67,20 +88,22 @@ void LoadBalancingManager::VirtualConnection::BeginSendRequest()
 	if (!inQueue_)
 	{
 		bool dataReady = false;
-		auto itr = sendSegments_.begin(), itrEnd = sendSegments_.begin() + std::min(sendSegments_.size(), sendWindow_);
+		auto itr = sendSegments_.begin(), itrEnd = sendSegments_.end();
 		while (!dataReady && itr != itrEnd)
 		{
-			const auto &sendingSegment = *itr;
-			switch (sendingSegment->state)
+			const auto &segment = *itr;
+			if (segment->segmentId - sendLaskAck_ > sendWindow_)
+				break;
+			switch (segment->state)
 			{
-			case SendRequest::READY:
+			case SendSegment::READY:
 				//Found, breaking
 				dataReady = true;
 				break;
-			case SendRequest::SENT:
+			case SendSegment::SENT:
 				++itr;
 				break;
-			case SendRequest::ACKNOWLEDGED: //Shoudn't happen
+			case SendSegment::ACKNOWLEDGED: //Shoudn't happen
 				assert(false);
 			}
 		}
@@ -88,101 +111,242 @@ void LoadBalancingManager::VirtualConnection::BeginSendRequest()
 			return;
 		inQueue_ = true;
 		lock.unlock();
-		parent_.AppendPendingSendRequest(virtualConnectionId_);
+		parent_.AppendPendingSendSegment(virtualConnectionId_);
 	}
 }
 
-void LoadBalancingManager::VirtualConnection::DoSendRequest(const std::shared_ptr<Connection> &connection)
+void LoadBalancingManager::VirtualConnection::DoSendSegment(const std::shared_ptr<Connection> &connection)
 {
 	std::unique_lock<std::recursive_mutex> lock(mutex_);
 	if (closed)
 		return;
+
 	inQueue_ = false;
 
-	std::shared_ptr<SendRequest> sendingSegment;
-	auto itr = sendSegments_.begin(), itrEnd = sendSegments_.begin() + std::min(sendSegments_.size(), sendWindow_);
+	std::shared_ptr<SendSegment> sendingSegment;
+	auto itr = sendSegments_.begin(), itrEnd = sendSegments_.end();
 	while (!sendingSegment && itr != itrEnd)
 	{
-		sendingSegment = *itr;
-		switch (sendingSegment->state)
-		{
-		case SendRequest::READY:
-			//Found, breaking
+		const auto &segment = *itr;
+		if (segment->segmentId - sendLaskAck_ > sendWindow_)
 			break;
-		case SendRequest::SENT:
-			sendingSegment.reset();
+		switch (segment->state)
+		{
+		case SendSegment::READY:
+			sendingSegment = segment;
+			//Erase if it's ACK since it doesn't need to be ACKed
+			if (sendingSegment->isAcknowledgement)
+				sendSegments_.erase(itr);
+			break;
+		case SendSegment::SENT:
 			++itr;
 			break;
-		case SendRequest::ACKNOWLEDGED: //Shoudn't happen
+		case SendSegment::ACKNOWLEDGED: //Shoudn't happen
 			assert(false);
 		}
 	}
+	bool sendingPureAck = false;
 	if (!sendingSegment)
 	{
-		//Make a Pure ACK segment
-		sendingSegment = std::make_shared<SendRequest>(ioContext_, true);
+		sendingPureAck = true;
+
+		//Make a ACK segment
+
+		uint32_t sendSegmentId = sendSegmentIdNext_;
+		sendingSegment = std::make_shared<SendSegment>(ioContext_, sendSegmentId, true);
 		sendingSegment->data.resize(16);
 		char *data = sendingSegment->data.data();
 		uint32_t virtualConnectionIdLE = boost::endian::native_to_little(virtualConnectionId_);
 		memcpy(data, &virtualConnectionIdLE, 4);
-		uint32_t sendSegmentIdNextLE = boost::endian::native_to_little(sendSegmentIdNext_++);
+		uint32_t sendSegmentIdNextLE = boost::endian::native_to_little(sendSegmentId);
 		memcpy(data + 4, &sendSegmentIdNextLE, 4);
-		data[12] = data[14] = data[15] = 0;
+		data[12] = ACK;
+		data[14] = data[15] = 0;
 	}
 
 	uint32_t ackLE = boost::endian::native_to_little(receiveSegmentIdComplete_);
-	uint8_t wndLE = boost::endian::native_to_little((uint8_t)(std::extent<decltype(receiveSegments_)>::value - receiveSegmentCount_));
 	assert(sendingSegment->data.size() >= 16);
 	memcpy(sendingSegment->data.data() + 8, &ackLE, 4);
-	sendingSegment->data[13] = wndLE;
+	sendingSegment->data[13] = (uint8_t)(std::extent<decltype(receiveSegments_)>::value - (receiveSegmentIdComplete_ - receiveSegmentOffset_));
 
-	sendingSegment->state = SendRequest::SENT;
+	sendingSegment->state = SendSegment::SENT;
 	++sendingSegment->tryCount;
-	if (!sendingSegment->isPureAck) //Pure ACK doesn't need retransmission
+	if (!sendingSegment->isAcknowledgement) //ACK doesn't need retransmission
 	{
-		sendingSegment->retryTimer.expires_after(kTimeRetry);
+		sendingSegment->retryTimer.expires_after(std::chrono::milliseconds(std::min(kTimeRetryMin, connection->RTT())));
 		sendingSegment->retryTimer.async_wait([this, self = shared_from_this(), sendingSegment, tryCount = sendingSegment->tryCount](const boost::system::error_code &ec)
 		{
 			if (!ec)
-				RetrySendRequest(sendingSegment, tryCount);
+				RetrySendSegment(sendingSegment, tryCount);
 		});
 	}
 
 	lock.unlock();
-	connection->AccessConnection()->async_write(const_buffer(sendingSegment->data),
+	connection->Socket()->async_write(const_buffer(sendingSegment->data),
 		[this, self = shared_from_this(), connection, sendingSegment, tryCount = sendingSegment->tryCount](error_code err)
 	{
 		if (err)
 		{
 			connection->SetConnectionFailed();
 			connection->ReleaseConnection();
-			RetrySendRequest(sendingSegment, tryCount);
+			RetrySendSegment(sendingSegment, tryCount);
 		}
 		else
 		{
 			connection->ReleaseConnection();
 		}
 	});
-	if (!sendingSegment->isPureAck) //Pure ACK only appears when nothing is ready
-		BeginSendRequest();
+	if (sendingSegment->data[12] & RST) //RST sent
+	{
+		ShutdownCheck();
+		return;
+	}
+	if (!sendingPureAck) //Pure ACK only appears when nothing is ready
+		BeginSendSegment();
 }
 
-void LoadBalancingManager::VirtualConnection::RetrySendRequest(const std::shared_ptr<SendRequest> &sendingSegment, size_t tryCount)
+void LoadBalancingManager::VirtualConnection::RetrySendSegment(const std::shared_ptr<SendSegment> &sendingSegment, size_t tryCount)
 {
-	if (sendingSegment->isPureAck) //Pure ACK doesn't need retransmission
+	if (sendingSegment->isAcknowledgement) //ACK doesn't need retransmission
 		return;
 	std::unique_lock<std::recursive_mutex> lock(mutex_);
 	if (closed)
 		return;
-	if (sendingSegment->state != SendRequest::SENT || sendingSegment->tryCount != tryCount)
+	if (sendingSegment->state != SendSegment::SENT || sendingSegment->tryCount != tryCount) //Prevents re-retransmission
 		return;
-	sendingSegment->state = SendRequest::READY;
+	sendingSegment->state = SendSegment::READY;
 	sendingSegment->retryTimer.cancel();
 	lock.unlock();
-	BeginSendRequest();
+	BeginSendSegment();
 }
 
-void LoadBalancingManager::AppendPendingSendRequest(uint32_t virtualConnectionId)
+void LoadBalancingManager::VirtualConnection::OnReceiveSegment(const char *data, size_t size)
+{
+	uint32_t seq, ack;
+	memcpy(&seq, data + 4, 4);
+	boost::endian::little_to_native_inplace(seq);
+	memcpy(&ack, data + 8, 4);
+	boost::endian::little_to_native_inplace(ack);
+	uint8_t flags = data[12];
+	uint8_t wnd = data[13];
+
+	std::unique_lock<std::recursive_mutex> lock(mutex_);
+	if (closed)
+		return;
+	if (shutdownFlags & SHUTDOWN_RECEIVE)
+	{
+		if (flags & RST) //Process only RST after SHUTDOWN_RECEIVE is set (FIN received)
+		{
+			shutdownFlags |= SHUTDOWN_COMPLETE;
+			ShutdownCheck();
+			return;
+		}
+		if (!(flags & ACK) && !inQueue_) //Other things are discarded, just send an ACK if needed
+		{
+			inQueue_ = true;
+			lock.unlock();
+			parent_.AppendPendingSendSegment(virtualConnectionId_);
+		}
+		return;
+	}
+
+	bool ackIsValid = sendSegments_.empty() ? (ack == sendLaskAck_) : (ack - sendLaskAck_ < 0x80000000 && sendSegments_.back()->segmentId - ack < 0x80000000);
+	if (ackIsValid)
+	{
+		auto itr = sendSegments_.begin(), itrEnd = sendSegments_.end();
+		for (; itr != itrEnd; ++itr)
+		{
+			const auto &segment = *itr;
+			if (ack - segment->segmentId >= 0x80000000)
+				break;
+			segment->state = SendSegment::ACKNOWLEDGED;
+			if (segment->data[12] & FIN)
+			{
+				assert(itr == --sendSegments_.end());
+				shutdownFlags |= SHUTDOWN_SENT;
+				ShutdownCheck();
+			}
+		}
+		sendSegments_.erase(sendSegments_.begin(), itr);
+		sendLaskAck_ = ack;
+		sendWindow_ = wnd;
+	}
+	else
+	{
+		if (ack - sendLaskAck_ >= 0x40000000 && ack - sendLaskAck_ < 0xC0000000)
+		{
+			lock.unlock();
+			Reset();
+			return;
+		}
+	}
+
+	//Process special flags
+	if (flags & RST)
+	{
+		shutdownFlags |= SHUTDOWN_COMPLETE;
+		ShutdownCheck();
+		return;
+	}
+	bool needAck = true;
+	if (flags & ACK)
+	{
+		needAck = false;
+	}
+	else
+	{
+		//Reassemble
+		size_t segmentPosition = seq - receiveSegmentOffset_;
+		if (segmentPosition < kMaxWindowSize)
+		{
+			auto &receiveSegment = receiveSegments_[segmentPosition];
+			if (receiveSegment.first)
+			{
+				if (receiveSegment.second.size() != size)
+				{
+					lock.unlock();
+					Reset();
+					return;
+				}
+			}
+			else
+			{
+				receiveSegment.first = true;
+				receiveSegments_->second.assign(data, data + size);
+				if (segmentPosition + 1 > receiveSegmentCount_)
+					receiveSegmentCount_ = segmentPosition + 1;
+
+				uint32_t i = 0;
+				for (; i < receiveSegmentCount_; ++i)
+					if (!receiveSegments_[i].first)
+						break;
+				receiveSegmentIdComplete_ = receiveSegmentOffset_ + i - 1;
+				if (receiveCallback_ && i > 0)
+				{
+					receiveCallback_(0);
+					receiveCallback_ = nullptr;
+				}
+			}
+		}
+	}
+
+	if (ackIsValid) //Valid ACK received, send queue and send window updated, check send callback
+	{
+		if (sendCallback_ && sendSegmentIdPending_ - sendLaskAck_ <= sendWindow_)
+		{
+			sendCallback_(0);
+			sendCallback_ = nullptr;
+		}
+	}
+	if (needAck && !inQueue_) //Send an ACK if needed
+	{
+		inQueue_ = true;
+		lock.unlock();
+		parent_.AppendPendingSendSegment(virtualConnectionId_);
+	}
+}
+
+void LoadBalancingManager::AppendPendingSendSegment(uint32_t virtualConnectionId)
 {
 	std::unique_lock<std::recursive_mutex> generalLock(generalMutex_);
 	if (!requestQueueSet_.insert(virtualConnectionId).second)
@@ -190,10 +354,13 @@ void LoadBalancingManager::AppendPendingSendRequest(uint32_t virtualConnectionId
 	bool needDispatch = requestQueue_.empty() && !idleConnections_.empty();
 	requestQueue_.push_back(virtualConnectionId);
 	if (needDispatch)
-		DispatchPendingSendRequest();
+	{
+		generalLock.unlock();
+		DispatchPendingSendSegment();
+	}
 }
 
-void LoadBalancingManager::DispatchPendingSendRequest()
+void LoadBalancingManager::DispatchPendingSendSegment()
 {
 	std::unique_lock<std::recursive_mutex> generalLock(generalMutex_);
 	if (requestQueue_.empty())
@@ -202,7 +369,7 @@ void LoadBalancingManager::DispatchPendingSendRequest()
 		return;
 
 	std::shared_ptr<Connection> connection = std::make_shared<Connection>(*this);
-	if (!connection->AccessConnection())
+	if (*connection)
 		return;
 
 	std::shared_ptr<VirtualConnection> virtualConnection;
@@ -221,50 +388,135 @@ void LoadBalancingManager::DispatchPendingSendRequest()
 		return;
 
 	generalLock.unlock();
-	virtualConnection->DoSendRequest(connection);
+	virtualConnection->DoSendSegment(connection);
 }
 
-LoadBalancingManager::BaseConnection LoadBalancingManager::OccupyConnection()
+void LoadBalancingManager::ReceiveSegment(const std::shared_ptr<BaseConnection> &receiveSocket)
+{
+	receiveSocket->socket->async_read(mutable_buffer(receiveSocket->receiveBuffer, 16),
+		[this, receiveSocket](error_code err)
+	{
+		if (err)
+		{
+			EndReceiveSegmentWithError(receiveSocket);
+			return;
+		}
+		uint16_t len;
+		memcpy(&len, receiveSocket->receiveBuffer + 14, 2);
+		boost::endian::little_to_native_inplace(len);
+		if (len == 0)
+		{
+			EndReceiveSegment(receiveSocket, len);
+			ReceiveSegment(receiveSocket);
+		}
+		else
+		{
+			receiveSocket->socket->async_read(mutable_buffer(receiveSocket->receiveBuffer + 16, len),
+				[this, receiveSocket, len](error_code err)
+			{
+				if (err)
+				{
+					EndReceiveSegmentWithError(receiveSocket);
+					return;
+				}
+				EndReceiveSegment(receiveSocket, len);
+				ReceiveSegment(receiveSocket);
+			});
+		}
+	});
+}
+
+void LoadBalancingManager::EndReceiveSegment(const std::shared_ptr<BaseConnection> &receiveSocket, size_t len)
+{
+	uint32_t vConnId;
+	memcpy(&vConnId, receiveSocket->receiveBuffer, 4);
+	std::shared_ptr<VirtualConnection> virtualConnection;
+
+	std::unique_lock<std::recursive_mutex> generalLock(generalMutex_);
+	auto itr = virtualConnections_.find(vConnId);
+	if (itr == virtualConnections_.end())
+	{
+		uint8_t flags = receiveSocket->receiveBuffer[12];
+		if (flags & RST)
+			return;
+		if (!(flags & SYN))
+		{
+			//TODO: make corresponding vConn and send RST
+			return;
+		}
+		//TODO: make corresponding vConn
+		generalLock.unlock();
+	}
+	else
+	{
+		virtualConnection = itr->second;
+		generalLock.unlock();
+	}
+	virtualConnection->OnReceiveSegment(receiveSocket->receiveBuffer, 16 + len);
+}
+
+void LoadBalancingManager::EndReceiveSegmentWithError(const std::shared_ptr<BaseConnection> &receiveSocket)
+{
+	std::unique_lock<std::recursive_mutex> generalLock(generalMutex_);
+	auto itr = idleConnections_.find(receiveSocket->index);
+	if (itr != idleConnections_.end())
+	{
+		idleConnections_.erase(itr);
+		generalLock.unlock();
+		receiveSocket->socket->async_close([this, receiveSocket](error_code)
+		{
+			ioContext_.post([this, receiveSocket]() { InitConnection(receiveSocket->index); });
+		});
+	}
+	else
+	{
+		generalLock.unlock();
+		receiveSocket->socket->async_close([this, receiveSocket](error_code) {});
+	}
+}
+
+std::shared_ptr<LoadBalancingManager::BaseConnection> LoadBalancingManager::OccupyConnection()
 {
 	std::lock_guard<std::recursive_mutex> generalLock(generalMutex_);
 	if (idleConnections_.empty())
-		return BaseConnection();
+		return std::shared_ptr<BaseConnection>();
 	auto itr = idleConnections_.begin();
-	BaseConnection ret = std::move(itr->second);
+	std::shared_ptr<BaseConnection> ret = std::move(itr->second);
 	idleConnections_.erase(itr);
 	return ret;
 }
 
-void LoadBalancingManager::ReleaseConnection(BaseConnection &&conn, bool failed)
+void LoadBalancingManager::ReleaseConnection(std::shared_ptr<BaseConnection> &&conn, bool failed)
 {
 	if (failed)
 	{
-		std::shared_ptr<BaseConnection> connection = std::make_shared<BaseConnection>(std::move(conn));
-		connection->connection->async_close([this, connection](error_code)
+		std::shared_ptr<BaseConnection> connection = std::move(conn);
+		connection->socket->async_close([this, connection](error_code)
 		{
 			ioContext_.post([this, connection]() { InitConnection(connection->index); });
 		});
 		return;
 	}
-	std::lock_guard<std::recursive_mutex> generalLock(generalMutex_);
+	std::unique_lock<std::recursive_mutex> generalLock(generalMutex_);
 	bool needDispatch = !requestQueue_.empty() && idleConnections_.empty();
-	idleConnections_.emplace(conn.index, std::move(conn));
+	if (!idleConnections_.emplace(conn->index, std::move(conn)).second)
+		return;
 	if (needDispatch)
-		DispatchPendingSendRequest();
+	{
+		generalLock.unlock();
+		DispatchPendingSendSegment();
+	}
 }
 
 void LoadBalancingManager::InitConnection(size_t index)
 {
 	NewBaseTcpSocket(index,
-		[this, index](error_code err, std::unique_ptr<prx_tcp_socket> &&socket)
+		[this, index](std::unique_ptr<prx_tcp_socket> &&socket, uint16_t rtt)
 	{
-		if (err)
-		{
-			InitConnection(index);
+		if (!socket)
 			return;
-		}
-		std::shared_ptr<prx_tcp_socket> shared_socket(std::move(socket));
-		BeginReceiveSegment(shared_socket);
-		ReleaseConnection(BaseConnection(index, std::move(shared_socket)), false);
+		std::shared_ptr<BaseConnection> baseConnection = std::make_shared<BaseConnection>(index, std::move(socket), rtt);
+		ReceiveSegment(baseConnection);
+		ReleaseConnection(std::move(baseConnection), false);
 	});
 }
--- LProxy/LoadBalancingManager.h
+++ LProxy/LoadBalancingManager.h
@@ -2,24 +2,27 @@
 
 class LoadBalancingManager
 {
-	static constexpr auto kTimeRetry = std::chrono::milliseconds(100);
+	static constexpr uint16_t kTimeRetryMin = 100;
 	enum
 	{
 		SYN = 0x01,
-		EST = 0x02,
-		FIN = 0x04,
-		RST = 0x08,
+		FIN = 0x02,
+		RST = 0x04,
+		ACK = 0x08,
 	};
 
 	struct BaseConnection
 	{
 		BaseConnection() = default;
-		BaseConnection(size_t idx, std::shared_ptr<prx_tcp_socket> &&conn) :index(idx), connection(std::move(conn)) {}
+		BaseConnection(size_t idx, std::unique_ptr<prx_tcp_socket> &&soc, uint16_t rtt_ms) :index(idx), socket(std::move(soc)), rtt(rtt_ms) {}
 		BaseConnection(const BaseConnection &) = delete;
 		BaseConnection(BaseConnection &&) = default;
 
 		size_t index = -1;
-		const std::shared_ptr<prx_tcp_socket> connection;
+		const std::unique_ptr<prx_tcp_socket> socket;
+		uint16_t rtt; //In milliseconds
+
+		char receiveBuffer[0x10010];
 	};
 
 	struct Connection
@@ -31,24 +34,37 @@ class LoadBalancingManager
 		}
 		Connection(const Connection &) = delete;
 		Connection(Connection &&) = default;
-		~Connection() { if (connection.connection) parent.ReleaseConnection(std::move(connection), connectionFailed); }
+		~Connection() { if (connection) parent.ReleaseConnection(std::move(connection), connectionFailed); }
 
-		prx_tcp_socket *AccessConnection() { return connection.connection.get(); }
+		operator bool() const { return (bool)connection; }
+		prx_tcp_socket *Socket() const { return connection->socket.get(); }
+		uint16_t RTT() const { return connection->rtt; }
 		void SetConnectionFailed() { connectionFailed = true; }
-		void ReleaseConnection() { parent.ReleaseConnection(std::move(connection), connectionFailed); }
+		void ReleaseConnection() { parent.ReleaseConnection(std::move(connection), connectionFailed); connection.reset(); }
 	private:
 		LoadBalancingManager &parent;
-		BaseConnection connection;
+		std::shared_ptr<BaseConnection> connection;
 		bool connectionFailed = false;
 	};
 
 	class VirtualConnection : std::enable_shared_from_this<VirtualConnection>
 	{
-		using SendRequestCallback = null_callback;
+		using SegmentCallback = null_callback;
 
-		struct SendRequest
+		static constexpr uint16_t kMaxWindowSize = 8;
+
+		enum
 		{
-			SendRequest(asio::io_context &ioCtx, bool isAcknowledgement = false) :retryTimer(ioCtx), isPureAck(isAcknowledgement) {}
+			SHUTDOWN_SEND     = 0x01,
+			SHUTDOWN_RECEIVE  = 0x02,
+			SHUTDOWN_BOTH     = SHUTDOWN_SEND | SHUTDOWN_RECEIVE,
+			SHUTDOWN_SENT     = 0x04,
+			SHUTDOWN_COMPLETE = SHUTDOWN_BOTH | SHUTDOWN_SENT,
+		};
+
+		struct SendSegment
+		{
+			SendSegment(asio::io_context &ioCtx, uint32_t segId, bool isAck) :retryTimer(ioCtx), segmentId(segId), isAcknowledgement(isAck) {}
 
 			enum
 			{
@@ -61,58 +77,75 @@ class LoadBalancingManager
 			boost::asio::steady_timer retryTimer;
 			size_t tryCount = 0;
 			uint8_t state = READY;
-			const bool isPureAck;
+
+			const uint32_t segmentId;
+			const bool isAcknowledgement;
 		};
 	public:
 		VirtualConnection(uint32_t vConnId, asio::io_context &ioCtx, LoadBalancingManager &parent_)
-			:virtualConnectionId_(vConnId), ioContext_(ioCtx), parent_(parent_)
+			:virtualConnectionId_(vConnId), ioContext_(ioCtx), parent_(parent_), shutdownTimer_(ioCtx)
 		{
 		}
-		~VirtualConnection() { if (sendCallback_) sendCallback_(ERR_OPERATION_FAILURE); }
+		~VirtualConnection() { if (sendCallback_) sendCallback_(ERR_OPERATION_FAILURE); if (receiveCallback_) receiveCallback_(ERR_OPERATION_FAILURE); }
 
 		uint32_t VirtualConnectionId() const { return virtualConnectionId_; }
 
-		void AppendSendRequest(uint8_t flags, const char *payload, uint16_t payloadSize, SendRequestCallback &&completeHandler);
-		void DoSendRequest(const std::shared_ptr<Connection> &connection);
+		void AppendSendSegment(uint8_t flags, const char *payload, uint16_t payloadSize, SegmentCallback &&completeHandler);
+		void DoSendSegment(const std::shared_ptr<Connection> &connection);
+
+		void OnReceiveSegment(const char *data, size_t size);
+
+		void Shutdown();
+		void Reset();
 	private:
-		void BeginSendRequest();
-		void RetrySendRequest(const std::shared_ptr<SendRequest> &sendingSegment, size_t tryCount);
+		void BeginSendSegment();
+		void RetrySendSegment(const std::shared_ptr<SendSegment> &sendingSegment, size_t tryCount);
+
+		void ShutdownCheck();
 
 		const uint32_t virtualConnectionId_;
 
-		std::vector<std::shared_ptr<SendRequest>> sendSegments_;
-		uint32_t sendSegmentIdNext_ = 0;
-		size_t sendWindow_ = 0;
-		SendRequestCallback sendCallback_; //Set if sendSegments_ is full before appended, Called after sendSegments_ is not full
+		std::vector<std::shared_ptr<SendSegment>> sendSegments_;
+		uint32_t sendSegmentIdNext_ = 0, sendLaskAck_ = -1;
+		uint8_t sendWindow_ = 1;
+		SegmentCallback sendCallback_; //Set if sendSegments_ is full before appended, Called after sendSegments_ is not full
+		uint32_t sendSegmentIdPending_ = -1;
 
-		std::pair<uint32_t, std::vector<char>> receiveSegments_[8];
+		std::pair<bool, std::vector<char>> receiveSegments_[kMaxWindowSize];
 		uint32_t receiveSegmentIdComplete_ = -1, receiveSegmentCount_ = 0;
-		size_t receiveSegmentOffset = 0;
-		//receiveCallback; //Blocks if receiveSegments_ is empty, Called after data is copied to dst
+		uint32_t receiveSegmentOffset_ = 0;
+		SegmentCallback receiveCallback_; //Blocks if receiveSegments_ is empty, Called after data is copied to dst
 
 		asio::io_context &ioContext_;
 		LoadBalancingManager &parent_;
 		std::recursive_mutex mutex_;
+		asio::steady_timer shutdownTimer_;
+		uint8_t shutdownFlags = 0;
 		bool inQueue_ = false, closed = false;
 	};
 public:
-	void AppendPendingSendRequest(uint32_t virtualConnectionId);
-	void DispatchPendingSendRequest();
 private:
-	void BeginReceiveSegment(const std::shared_ptr<prx_tcp_socket> &receiveSocket);
+	void AppendPendingSendSegment(uint32_t virtualConnectionId);
+	void DispatchPendingSendSegment();
 
-	BaseConnection OccupyConnection();
-	void ReleaseConnection(BaseConnection &&conn, bool failed);
+	void ReceiveSegment(const std::shared_ptr<BaseConnection> &receiveSocket);
+	void EndReceiveSegment(const std::shared_ptr<BaseConnection> &receiveSocket, size_t len);
+	void EndReceiveSegmentWithError(const std::shared_ptr<BaseConnection> &receiveSocket);
+
+	void OnVirtualConnectionClosed(uint32_t virtualConnectionId);
+
+	std::shared_ptr<BaseConnection> OccupyConnection();
+	void ReleaseConnection(std::shared_ptr<BaseConnection> &&conn, bool failed);
 	void InitConnection(size_t index);
 
 	virtual size_t NewBaseTcpSocketMaxIndex() const = 0;
-	virtual void NewBaseTcpSocket(size_t index, prx_listener::accept_callback &&completeHandler) const = 0;
+	virtual void NewBaseTcpSocket(size_t index, std::function<void(std::unique_ptr<prx_tcp_socket> &&, uint16_t)> &&completeHandler) const = 0;
 
 	asio::io_context &ioContext_;
 
 	std::recursive_mutex generalMutex_;
 	std::deque<uint32_t> requestQueue_;
 	std::unordered_set<uint32_t> requestQueueSet_;
-	std::unordered_map<uint32_t, BaseConnection> idleConnections_;
+	std::unordered_map<uint32_t, std::shared_ptr<BaseConnection>> idleConnections_;
 	std::unordered_map<uint32_t, std::shared_ptr<VirtualConnection>> virtualConnections_;
 };
